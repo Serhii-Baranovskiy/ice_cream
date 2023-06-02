586d04030b138b702b51a15663ff72dd6a1e043c
Serhii-Baranovskiy <sibaranovskiy@gmail.com>
2023-06-02 17:43:16 +0300

    часткова модалка product

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,33 @@
 (() => {
     const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-      open: document.querySelector("[open-modal]"),
+      openModalHeaderBtn: document.querySelector("[data-modal-header-open]"),
+      closeModalHeaderBtn: document.querySelector("[data-modal-header-close]"),
+      modalHeader: document.querySelector("[data-modal-header]"),
+      openHeader: document.querySelector("[open-modal-header]"),
+
+      openModalProductBtnOne: document.querySelector("[data-modal-product-open-first]"),
+      openModalProductBtnTwo: document.querySelector("[data-modal-product-open-second]"),
+      openModalProductBtnThree: document.querySelector("[data-modal-product-open-third]"),
+      closeModalProductBtn: document.querySelector("[data-modal-product-close]"),
+      modalProduct: document.querySelector("[data-modal-product]"),
+      openProduct: document.querySelector("[open-modal-product]"),
     };
   
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
+    refs.openModalHeaderBtn.addEventListener("click", toggleModalHeader);
+    refs.closeModalHeaderBtn.addEventListener("click", toggleModalHeader);
+
+    refs.openModalProductBtnOne.addEventListener("click", toggleModalProduct);
+    refs.openModalProductBtnTwo.addEventListener("click", toggleModalProduct);
+    refs.openModalProductBtnThree.addEventListener("click", toggleModalProduct);
+    refs.closeModalProductBtn.addEventListener("click", toggleModalProduct);
   
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-      refs.open.classList.toggle("is-open");
+    function toggleModalHeader() {
+      refs.modalHeader.classList.toggle("is-hidden-modal-header");
+      refs.openHeader.classList.toggle("is-open-modal-header");
+    }
+
+    function toggleModalProduct() {
+      refs.modalProduct.classList.toggle("is-hidden-modal-product");
+      refs.openProduct.classList.toggle("is-open-modal-product");
     }
   })();
